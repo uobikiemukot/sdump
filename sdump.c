9e5693ada92cee030e55508d9d4de372e26b373d
uobikiemukot <uobikiemukot@gmail.com>
2014-09-29 16:09:44 +0900

    move sixel related code to sixel.h

--- sdump.c
+++ sdump.c
@@ -3,16 +3,7 @@
 #include "util.h"
 #include "loader.h"
 #include "image.h"
-
-enum {
-	SIXEL_COLORS = 256,
-	SIXEL_BPP    = 3,
-	TERM_WIDTH   = 1280,
-	TERM_HEIGHT  = 1024,
-	BUFSIZE      = 1024,
-};
-
-char temp_file[PATH_MAX];
+#include "sixel.h"
 
 void usage()
 {
@@ -77,33 +68,9 @@ char *make_temp_file(const char *template)
 	return temp_file;
 }
 
-int sixel_write_callback(char *data, int size, void *priv)
+void cleanup(struct sixel_t *sixel, struct image *img)
 {
-	/*
-	for (int i = 0; i < size; i++) {
-		if (i == (size - 1))
-			break;
-
-		if (*(data + i) == 0x1B && *(data + i + 1) == 0x5C) {
-			fprintf((FILE *) priv, "\033\033\\\033P\\");
-			break;
-		} else {
-			fwrite(data + i, 1, 1, fp);
-		}
-	}
-
-	logging(DEBUG, "write callback() size:%d\n", size);
-	return size;
-	*/
-	return fwrite(data, size, 1, (FILE *) priv);
-}
-
-void cleanup(sixel_dither_t *sixel_dither, sixel_output_t *sixel_context, struct image *img)
-{
-	if (sixel_dither)
-		sixel_dither_unref(sixel_dither);
-	if (sixel_context)
-		sixel_output_unref(sixel_context);
+	sixel_die(sixel);
 	free_image(img);
 }
 
@@ -113,9 +80,14 @@ int main(int argc, char **argv)
 	char *file;
 	bool resize = false;
 	int angle = 0, opt;
+	struct winsize ws;
 	struct image img;
-	sixel_output_t *sixel_context = NULL;
-	sixel_dither_t *sixel_dither = NULL;
+	struct tty_t tty = {
+		.cell_width = CELL_WIDTH, .cell_height = CELL_HEIGHT,
+	};;
+	struct sixel_t sixel = {
+		.context = NULL, .dither = NULL,
+	};
 
 	/* check arg */
 	while ((opt = getopt(argc, argv, "hfr:")) != -1) {
@@ -154,57 +126,33 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws)) {
+		logging(ERROR, "ioctl: TIOCGWINSZ failed\n");
+		tty.width  = tty.cell_width * 80;
+		tty.height = tty.cell_height * 24;
+	} else {
+		tty.width  = tty.cell_width * ws.ws_col;
+		tty.height = tty.cell_height * ws.ws_row;
+	}
+
 	/* rotate/resize and draw */
 	/* TODO: support color reduction for 8bpp mode */
 	if (angle != 0)
 		rotate_image(&img, angle, true);
 
 	if (resize)
-		resize_image(&img, TERM_WIDTH, TERM_HEIGHT, true);
+		resize_image(&img, tty.width, tty.height, true);
 
 	/* sixel */
-	/* XXX: libsixel only allows 3 bytes per pixel image,
-		we should convert bpp when bpp is 1 or 2 or 4 */
-	if (get_image_channel(&img) != SIXEL_BPP)
-		normalize_bpp(&img, SIXEL_BPP, true);
-
-	if ((sixel_dither = sixel_dither_create(SIXEL_COLORS)) == NULL) {
-		logging(ERROR, "couldn't create dither\n");
+	if (!sixel_init(&tty, &sixel, &img))
 		goto error_occured;
-	}
-
-	/* XXX: use first frame for dither initialize */
-	if (sixel_dither_initialize(sixel_dither, get_current_frame(&img),
-		get_image_width(&img), get_image_height(&img),
-		SIXEL_BPP, LARGE_AUTO, REP_AUTO, QUALITY_AUTO) != 0) {
-		logging(ERROR, "couldn't initialize dither\n");
-		sixel_dither_unref(sixel_dither);
-		goto error_occured;
-	}
-	sixel_dither_set_diffusion_type(sixel_dither, DIFFUSE_AUTO);
-
-	if ((sixel_context = sixel_output_create(sixel_write_callback, stdout)) == NULL) {
-		logging(ERROR, "couldn't create sixel context\n");
-		goto error_occured;
-	}
-	sixel_output_set_8bit_availability(sixel_context, CSIZE_7BIT);
-
-	printf("\0337"); /* save cursor position */
-	//printf("\033[s");
-	for (int i = 0; i < get_frame_count(&img); i++) {
-		printf("\0338"); /* restore cursor position */
-		//printf("\033[u");
-		sixel_encode(get_current_frame(&img), get_image_width(&img), get_image_height(&img),
-			get_image_channel(&img), sixel_dither, sixel_context);
-		usleep(get_current_delay(&img) * 10000); /* gif delay 1 == 1/100 sec */
-		increment_frame(&img);
-	}
+	sixel_write(&tty, &sixel, &img);
 
 	/* cleanup resource */
-	cleanup(sixel_dither, sixel_context, &img);
+	cleanup(&sixel, &img);
 	return EXIT_SUCCESS;
 
 error_occured:
-	cleanup(sixel_dither, sixel_context, &img);
+	cleanup(&sixel, &img);
 	return EXIT_FAILURE;;
 }
